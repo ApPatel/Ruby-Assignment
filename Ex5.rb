7070b20fbab17cf50c2a47c6f7b693551295ce1d
Atul Patel <patel.atul10@gmail.com>
2012-12-12 21:14:36 +0530

    Ruby assignment 5

--- Ex5.rb
+++ Ex5.rb
@@ -1,16 +1,10 @@
+def sum_concate (arg1, arg2)
 
-
-
-puts "Enter 1st argument"
-arg1 = gets
-puts "Enter 2nd argument"
-arg2 = gets
-
-
-puts arg1.class
-puts arg2.class
-
-if (arg1.to_i)
-	puts "111111"
-	puts arg1
+begin
+   arg1 + arg2
+rescue
+   puts "Cannot not add/concate arguments arg1 (#{arg1.class}) and arg2 (#{arg2.class})"
+else
+   puts "#{arg1 + arg2}"
+end
 end
