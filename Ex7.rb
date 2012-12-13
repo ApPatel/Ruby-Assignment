0cf575dad3d24c84874d4613a3668d68bd000aaa
Atul Patel <patel.atul10@gmail.com>
2012-12-13 14:27:58 +0530

    Ruby assignment 7

--- Ex7.rb
+++ Ex7.rb
@@ -41,6 +41,10 @@ avanger= Bike.new(2,"Black", "80,000", "Bajaj")
 avanger.use
 avanger.class_name
 
+cruze= Car.new(4,"Grey", "15,50,000", "Chevrolet")
+cruze.use
+cruze.class_name
+
 volvo = Bus.new(10, "White", "30,00,000", "TATA")
 volvo.use
 volvo.class_name
